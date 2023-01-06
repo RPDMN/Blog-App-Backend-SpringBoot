f31f0992e9d5c9b390b1243a8f37558f3fdbcacf
ripudamansingh <ripud60@gmail.com>
2023-01-06 23:31:44 +0530

    6/1/23

--- src/main/java/com/ripudamanSingh/blog/blogappapi/BlogAppApiApplication.java
+++ src/main/java/com/ripudamanSingh/blog/blogappapi/BlogAppApiApplication.java
@@ -37,11 +37,11 @@ public class BlogAppApiApplication implements CommandLineRunner {
 		try{
 			Role role = new Role();
 			role.setId(AppConstants.ADMIN_USER);
-			role.setName("ADMIN_USER");
+			role.setName("ROLE_ADMIN");
 
 			Role role1 = new Role();
 			role1.setId(AppConstants.NORMAL_USER);
-			role1.setName("NORMAL_USER");
+			role1.setName("ROLE_NORMAL");
 
 
 			List<Role> roles = List.of(role,role1);
--- src/main/java/com/ripudamanSingh/blog/blogappapi/config/SwaggerConfig.java
+++ src/main/java/com/ripudamanSingh/blog/blogappapi/config/SwaggerConfig.java
@@ -1,23 +1,42 @@
 package com.ripudamanSingh.blog.blogappapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
+    public static final String AUTHORIZATION_HEADER="Authorization";
+
+    private ApiKey apiKeys(){
+        return new ApiKey("JWT", AUTHORIZATION_HEADER,"header");
+    }
+
+    private List<SecurityContext> securityContext(){
+        return Arrays.asList(SecurityContext.builder().securityReferences(sf()).build());
+    }
+
+    private List<SecurityReference> sf(){
+        AuthorizationScope scope = new AuthorizationScope("global","accessEveryThing");
+
+        return Arrays.asList(new SecurityReference("JWT",new AuthorizationScope[]{scope}));
+    }
 
     @Bean
     public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(getInfo()).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(getInfo()).securityContexts(securityContext()).securitySchemes(Arrays.asList(apiKeys())).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
     }
 
     private ApiInfo getInfo(){
